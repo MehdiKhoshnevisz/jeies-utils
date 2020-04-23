9904c3a66ae3e6244dd5f20a4ddc08e1347ed182
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-04-23 11:58:53 +0430

    add numberSeperator

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-export function doSomething() {
-  return 'Ha Ha!';
+export function numberSeperator(value) {
+  if (value === null || value === undefined) return 0;
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }

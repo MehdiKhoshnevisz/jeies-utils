f87f6a29ab131a8e3e0ae4cb8063d390c80cebde
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2020-05-02 16:56:39 +0430

    Add objectWithoutKeys util

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ export function numberSeperator(value) {
   if (value === null || value === undefined) return 0;
   return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+export function objectWithoutKeys(obj, keys, primitive = true) {
+  if (!obj) return {};
+  if (!keys) return obj;
+
+  let newObj = {};
+
+  if (primitive) {
+    newObj = { ...obj };
+  } else {
+    newObj = obj;
+  }
+
+  keys.forEach(key => {
+    delete newObj[key];
+  });
+
+  return newObj;
+}
